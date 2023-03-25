8c8cbabb13fc4fa3ea3cab93b10484a6ea5960c1
Karoline Siarsky <karoline@siarsky.ch>
2023-03-25 21:58:34 +0100

    SonarQube deployment failed because of one test

--- src/test/java/ch/uzh/ifi/hase/soprafs23/service/UserServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/service/UserServiceIntegrationTest.java
@@ -61,7 +61,7 @@ public class UserServiceIntegrationTest {
     User testUser = new User();
     testUser.setPassword("testPass");
     testUser.setUsername("testUsername");
-    //User createdUser = userService.createUser(testUser);
+    User createdUser = userService.createUser(testUser);
 
     // attempt to create second user with same username
     User testUser2 = new User();
