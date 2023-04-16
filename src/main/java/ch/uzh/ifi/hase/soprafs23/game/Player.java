e2f7a45317da945b122a8a29dd982fde5e26dc69
jverho <82749396+jverho@users.noreply.github.com>
2023-04-16 15:21:19 +0200

    added some methods to player #42

--- src/main/java/ch/uzh/ifi/hase/soprafs23/game/Player.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/game/Player.java
@@ -1,45 +1,55 @@
 package ch.uzh.ifi.hase.soprafs23.game;
+
 import ch.uzh.ifi.hase.soprafs23.game.blocks.*;
 
 public class Player {
     private Block[] blocks;
     private Inventory inventory;
+    private final CellStatus status;
 
     public Player(CellStatus cellStatus) {
         // Constructor logic here
+        this.status = cellStatus;
         this.blocks = new Block[] {
-            new Block1(this, cellStatus),
-            new Block2(this, cellStatus),
-            new Block3(this, cellStatus),
-            new Block4(this, cellStatus),
-            new Block5(this, cellStatus),
-            new Block6(this, cellStatus),
-            new Block7(this, cellStatus),
-            new Block8(this, cellStatus),
-            new Block9(this, cellStatus),
-            new Block10(this, cellStatus),
-            new Block11(this, cellStatus),
-            new Block12(this, cellStatus),
-            new Block13(this, cellStatus),
-            new Block14(this, cellStatus),
-            new Block15(this, cellStatus),
-            new Block16(this, cellStatus),
-            new Block17(this, cellStatus),
-            new Block18(this, cellStatus),
-            new Block19(this, cellStatus),
-            new Block20(this, cellStatus),
-            new Block21(this, cellStatus)
+                new Block1(this, cellStatus),
+                new Block2(this, cellStatus),
+                new Block3(this, cellStatus),
+                new Block4(this, cellStatus),
+                new Block5(this, cellStatus),
+                new Block6(this, cellStatus),
+                new Block7(this, cellStatus),
+                new Block8(this, cellStatus),
+                new Block9(this, cellStatus),
+                new Block10(this, cellStatus),
+                new Block11(this, cellStatus),
+                new Block12(this, cellStatus),
+                new Block13(this, cellStatus),
+                new Block14(this, cellStatus),
+                new Block15(this, cellStatus),
+                new Block16(this, cellStatus),
+                new Block17(this, cellStatus),
+                new Block18(this, cellStatus),
+                new Block19(this, cellStatus),
+                new Block20(this, cellStatus),
+                new Block21(this, cellStatus)
         };
 
         // Add all the blocks of the player to the inventory
         for (Block block : blocks) {
             inventory.addBlock(block);
         }
-        
-    }
 
+    }
     public Block[] getBlocks() {
         return blocks;
     }
-    
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+
+    public CellStatus getStatus() {
+        return status;
+    }
 }
