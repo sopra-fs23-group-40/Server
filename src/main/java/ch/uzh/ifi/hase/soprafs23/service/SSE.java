8eceeefa12f5906616b7cab119c67a42823cab0b
Karoline Siarsky <karoline@siarsky.ch>
2023-04-27 15:19:45 +0200

    trying to add @CrossOrigin annotation

--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/SSE.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/SSE.java
@@ -10,6 +10,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class SSE {
 
     private final LobbyHandler processor;
