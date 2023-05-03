6273cdf826d147306e671d21fa82d6b72ee70edb
jverho <82749396+jverho@users.noreply.github.com>
2023-05-03 11:32:45 +0200

    created tests for all LobbyController endpoints #78

--- src/test/java/ch/uzh/ifi/hase/soprafs23/controller/LobbyControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/LobbyControllerTest.java
@@ -1,8 +1,10 @@
 package ch.uzh.ifi.hase.soprafs23.controller;
 
+import ch.uzh.ifi.hase.soprafs23.constant.LobbyStatus;
 import ch.uzh.ifi.hase.soprafs23.constant.LobbyType;
 import ch.uzh.ifi.hase.soprafs23.entity.Lobby;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
+import ch.uzh.ifi.hase.soprafs23.rest.dto.LobbyPutDTO;
 import ch.uzh.ifi.hase.soprafs23.rest.dto.UserAuthDTO;
 import ch.uzh.ifi.hase.soprafs23.service.LobbyService;
 import ch.uzh.ifi.hase.soprafs23.service.SSE;
@@ -11,6 +13,7 @@ import ch.uzh.ifi.hase.soprafs23.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,6 +23,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -45,23 +53,33 @@ class LobbyControllerTest {
     @MockBean
     private SSE sse;
 
+    private User createTestUser(){
+        User createdUser = new User();
+        createdUser.setUsername("testUsername");
+        createdUser.setToken("testToken");
+        return createdUser;
+    }
+
+    private Lobby createTestLobby(){
+        Lobby createdLobby = new Lobby();
+        createdLobby.setLobbyId(1L);
+        createdLobby.setName("Test Lobby");
+        createdLobby.setHost("testUsername");
+        return createdLobby;
+    }
+
     @Test
     public void createLobby_Successful_ReturnStatusOK() throws Exception {
         // given
-        User user = new User();
-        user.setUsername("testUsername");
-        user.setToken("validToken");
-        userService.createUser(user);
+        User testUser = createTestUser();
+        userService.createUser(testUser);
 
-        Lobby lobby = new Lobby();
-        lobby.setLobbyId(1L);
-        lobby.setName("Test Lobby");
-        lobby.setHost(user.getUsername());
-        given(lobbyService.createLobby(user.getUsername())).willReturn(lobby);
+        Lobby testLobby = createTestLobby();
+        given(lobbyService.createLobby(testUser.getUsername())).willReturn(testLobby);
 
         UserAuthDTO userAuthDTO = new UserAuthDTO();
-        userAuthDTO.setUsername(user.getUsername());
-        userAuthDTO.setToken(user.getToken());
+        userAuthDTO.setUsername(testUser.getUsername());
+        userAuthDTO.setToken(testUser.getToken());
 
         // when
         MockHttpServletRequestBuilder postRequest = post("/createLobby")
@@ -77,21 +95,16 @@ class LobbyControllerTest {
     @Test
     public void changeLobbyType_Successful_ReturnStatusOK() throws Exception {
         // given
-        User user = new User();
-        user.setUsername("testUsername");
-        user.setToken("validToken");
-        userService.createUser(user);
+        User testUser = createTestUser();
+        userService.createUser(testUser);
 
-        Lobby lobby = new Lobby();
-        lobby.setLobbyId(1L);
-        lobby.setName("Test Lobby");
-        lobby.setHost(user.getUsername());
-        lobby.setLobbyType(LobbyType.PUBLIC);
-        given(lobbyService.changeLobbytype(user.getUsername(), lobby.getLobbyId())).willReturn(LobbyType.PRIVATE);
+        Lobby testLobby = createTestLobby();
+        testLobby.setLobbyType(LobbyType.PUBLIC);
+        given(lobbyService.changeLobbytype(testUser.getUsername(), testLobby.getLobbyId())).willReturn(LobbyType.PRIVATE);
 
         UserAuthDTO userAuthDTO = new UserAuthDTO();
-        userAuthDTO.setUsername(user.getUsername());
-        userAuthDTO.setToken(user.getToken());
+        userAuthDTO.setUsername(testUser.getUsername());
+        userAuthDTO.setToken(testUser.getToken());
 
         // when
         MockHttpServletRequestBuilder putRequest = put("/lobbytype/1")
@@ -108,12 +121,10 @@ class LobbyControllerTest {
     public void isHost_Valid_ReturnStatusOK() throws Exception {
         // given
         Long lobbyId = 1L;
-        String token = "validToken";
+        String token = "testToken";
         String username = "testUsername";
-        User user = new User();
-        user.setUsername(username);
-        user.setToken(token);
-        userService.createUser(user);
+        User testUser = createTestUser();
+        userService.createUser(testUser);
         given(lobbyService.checkIfHost(username, lobbyId)).willReturn(true);
 
         UserAuthDTO userAuthDTO = new UserAuthDTO();
@@ -134,38 +145,140 @@ class LobbyControllerTest {
     @Test
     public void testGetLobby() throws Exception {
         // given
-        User user = new User();
-        user.setUsername("testUsername");
-        user.setToken("validToken");
-        userService.createUser(user);
+        User testUser = createTestUser();
+        userService.createUser(testUser);
 
-        Lobby lobby = new Lobby();
-        lobby.setLobbyId(1L);
-        lobby.setName("Test Lobby");
-        lobby.setHost(user.getUsername());
-        lobby.setLobbyType(LobbyType.PUBLIC);
-        lobbyService.createLobby(user.getUsername());
-        given(lobbyService.getLobby(1L)).willReturn(lobby);
+        Lobby testLobby = createTestLobby();
+        testLobby.setLobbyType(LobbyType.PUBLIC);
+        lobbyService.createLobby(testUser.getUsername());
+        given(lobbyService.getLobby(1L)).willReturn(testLobby);
 
         UserAuthDTO userAuthDTO = new UserAuthDTO();
-        userAuthDTO.setUsername(user.getUsername());
-        userAuthDTO.setToken(user.getToken());
-
-        //given(lobbyService.checkIfHost(userAuthDTO.getUsername(), 1L)).willReturn(true);
+        userAuthDTO.setUsername(testUser.getUsername());
+        userAuthDTO.setToken(testUser.getToken());
 
         // when
         MockHttpServletRequestBuilder getRequest = get("/lobby/1")
-                .header("token", user.getToken())
-                .header("username", user.getUsername())
+                .header("token", testUser.getToken())
+                .header("username", testUser.getUsername())
                 .contentType(MediaType.APPLICATION_JSON);
 
         // then
         mockMvc.perform(getRequest)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.lobbyId").value(1))
-                .andExpect(jsonPath("$.name").value(lobby.getName()))
+                .andExpect(jsonPath("$.name").value(testLobby.getName()))
                 .andExpect(jsonPath("$.lobbyType").value("PUBLIC"));
     }
+
+    @Test
+    public void testGetLobbies() throws Exception {
+        // given
+        User user = createTestUser();
+        userService.createUser(user);
+
+        List<Lobby> lobbies = new ArrayList<>();
+        Lobby testLobby1 = createTestLobby();
+        testLobby1.setName("Test Lobby 1");
+        testLobby1.setStatus(LobbyStatus.WAITING);
+        lobbies.add(testLobby1);
+
+        Lobby lobby2 = createTestLobby();
+        lobby2.setLobbyId(2L);
+        lobby2.setName("Test Lobby 2");
+        lobby2.setStatus(LobbyStatus.IN_GAME);
+        lobbies.add(lobby2);
+
+        given(lobbyService.getLobbies()).willReturn(lobbies);
+
+        // when
+        MockHttpServletRequestBuilder getRequest = get("/lobbies")
+                .header("token", user.getToken())
+                .contentType(MediaType.APPLICATION_JSON);
+
+        // then
+        mockMvc.perform(getRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].lobbyId").value(1))
+                .andExpect(jsonPath("$[0].name").value("Test Lobby 1"));
+    }
+
+    @Test
+    public void testJoinLobby() throws Exception {
+        // given
+        User testUser = createTestUser();
+        userService.createUser(testUser);
+
+        LobbyPutDTO lobbyPutDTO = new LobbyPutDTO();
+        lobbyPutDTO.setId(1L);
+        lobbyPutDTO.setPasscode("1234");
+
+        UserAuthDTO userAuthDTO = new UserAuthDTO();
+        userAuthDTO.setUsername(testUser.getUsername());
+        userAuthDTO.setToken(testUser.getToken());
+
+        // when
+        MockHttpServletRequestBuilder postRequest = post("/joinLobby")
+                .header("token", testUser.getToken())
+                .header("username", testUser.getUsername())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(lobbyPutDTO));
+
+        // then
+        mockMvc.perform(postRequest)
+                .andExpect(status().isAccepted());
+
+        Mockito.verify(lobbyService, Mockito.times(1)).joinLobby(eq(1L), eq("1234"), eq(testUser.getUsername()));
+    }
+
+    @Test
+    public void testDeleteLobby() throws Exception {
+        // given
+        User testUser = createTestUser();
+        userService.createUser(testUser);
+
+        Lobby testLobby = createTestLobby();
+        given(lobbyService.getLobby(1L)).willReturn(testLobby);
+
+        UserAuthDTO userAuthDTO = new UserAuthDTO();
+        userAuthDTO.setUsername(testUser.getUsername());
+        userAuthDTO.setToken(testUser.getToken());
+
+        // when
+        MockHttpServletRequestBuilder deleteRequest = delete("/deletelobby/1")
+                .header("token", testUser.getToken())
+                .header("username", testUser.getUsername())
+                .contentType(MediaType.APPLICATION_JSON);
+
+        // then
+        mockMvc.perform(deleteRequest)
+                .andExpect(status().isOk());
+
+        Mockito.verify(lobbyService, Mockito.times(1)).deleteLobby(testUser.getUsername(), 1L);
+    }
+
+    @Test
+    public void testLeaveLobby() throws Exception {
+        // given
+        String username = "testUsername";
+        String token = "validToken";
+        UserAuthDTO userAuthDTO = new UserAuthDTO();
+        userAuthDTO.setUsername(username);
+        userAuthDTO.setToken(token);
+
+        // when
+        MockHttpServletRequestBuilder putRequest = put("/leavelobby/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(userAuthDTO));
+
+        // then
+        mockMvc.perform(putRequest)
+                .andExpect(status().isOk());
+
+        Mockito.verify(lobbyService).leaveLobby(userAuthDTO.getUsername(), 1L);
+    }
+
 
     private String asJsonString(final Object object) {
         try {
