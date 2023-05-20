524a4eb9875b8999bc9a3eb9241379ef399b4f28
jverho <82749396+jverho@users.noreply.github.com>
2023-05-20 10:13:47 +0200

    created tests for the remaining UserController endpoints #113

--- src/test/java/ch/uzh/ifi/hase/soprafs23/controller/UserControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/UserControllerTest.java
@@ -1,7 +1,9 @@
 package ch.uzh.ifi.hase.soprafs23.controller;
 
 import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs23.entity.Statistics;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
+import ch.uzh.ifi.hase.soprafs23.rest.dto.StatsGetDTO;
 import ch.uzh.ifi.hase.soprafs23.rest.dto.UserPostDTO;
 import ch.uzh.ifi.hase.soprafs23.service.TokenAuthenticationService;
 import ch.uzh.ifi.hase.soprafs23.service.UserService;
@@ -24,10 +26,10 @@ import java.util.List;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * UserControllerTest
@@ -49,7 +51,7 @@ public class UserControllerTest {
 
 
   @Test
-  public void givenUsers_whenGetUsers_thenReturnJsonArray() throws Exception {
+   void givenUsers_whenGetUsers_thenReturnJsonArray() throws Exception {
     // given
     User user = new User();
     user.setPassword("Password");
@@ -76,7 +78,7 @@ public class UserControllerTest {
   }
 
   @Test
-  public void createUser_validInput_userCreated() throws Exception {
+   void createUser_validInput_userCreated() throws Exception {
     // given
     User user = new User();
     user.setId(1L);
@@ -104,7 +106,103 @@ public class UserControllerTest {
         .andExpect(jsonPath("$.status", is(user.getStatus().toString())));
   }
 
-  /**
+    @Test
+     void loginUser_validInput_userLoggedIn() throws Exception {
+        // given
+        UserPostDTO userPostDTO = new UserPostDTO();
+        userPostDTO.setPassword("Test User");
+        userPostDTO.setUsername("testUsername");
+
+        User loggedInUser = new User();
+        loggedInUser.setId(1L);
+        loggedInUser.setPassword("Test User");
+        loggedInUser.setUsername("testUsername");
+        loggedInUser.setToken("12345");
+        loggedInUser.setStatus(UserStatus.ONLINE);
+        given(userService.loginUser(Mockito.any())).willReturn(loggedInUser);
+
+        // when
+        MockHttpServletRequestBuilder postRequest = post("/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(userPostDTO));
+
+        // then
+        mockMvc.perform(postRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(loggedInUser.getId().intValue())))
+                .andExpect(jsonPath("$.username", is(loggedInUser.getUsername())))
+                .andExpect(jsonPath("$.status", is(loggedInUser.getStatus().toString())))
+                .andExpect(header().exists("token"));
+    }
+
+    @Test
+     void logoutUser_validToken_userLoggedOut() throws Exception {
+        // given
+        User user = new User();
+        user.setUsername("testUsername");
+        user.setToken("testToken");
+        userService.createUser(user);
+        given(tokenAuthenticationService.checkToken(user.getToken())).willReturn(true);
+
+        // when
+        MockHttpServletRequestBuilder postRequest = post("/logout")
+                .header("token", user.getToken());
+
+        // then
+        mockMvc.perform(postRequest)
+                .andExpect(status().isOk());
+        verify(userService).logoutUser(user.getToken());
+    }
+
+    @Test
+     void getLoggedInUsername_validToken_returnUsername() throws Exception {
+        // given
+        User user = new User();
+        user.setUsername("testUsername");
+        user.setToken("testToken");
+        userService.createUser(user);
+        given(tokenAuthenticationService.checkToken(user.getToken())).willReturn(true);
+        given(userService.getLoggedInUsername(user.getToken())).willReturn(user.getUsername());
+
+        // when
+        MockHttpServletRequestBuilder getRequest = get("/loggedInName")
+                .header("token", user.getToken());
+
+        // then
+        mockMvc.perform(getRequest)
+                .andExpect(status().isOk())
+                .andExpect(content().string(user.getUsername()));
+    }
+
+    @Test
+     void getUserStatistics_validUsername_returnStatistics() throws Exception {
+        // given
+        String username = "testUsername";
+        StatsGetDTO expectedStatsGetDTO = new StatsGetDTO();
+        expectedStatsGetDTO.setBlocksPlaced(10);
+        expectedStatsGetDTO.setGamesPlayed(1);
+        expectedStatsGetDTO.setGamesWon(0);
+
+        Statistics userStatistics = new Statistics();
+        userStatistics.setBlocksPlaced(10);
+        userStatistics.setGamesPlayed(1);
+        userStatistics.setGamesWon(0);
+        given(userService.getStatistics(username)).willReturn(userStatistics);
+
+        // when
+        MockHttpServletRequestBuilder getRequest = get("/statistics")
+                .header("username", username);
+
+        // then
+        mockMvc.perform(getRequest)
+                .andExpect(status().isOk())
+                .andExpect(content().json(asJsonString(expectedStatsGetDTO)));
+    }
+
+
+
+
+    /**
    * Helper Method to convert userPostDTO into a JSON string such that the input
    * can be processed
    * Input will look like this: {"name": "Test User", "username": "testUsername"}
